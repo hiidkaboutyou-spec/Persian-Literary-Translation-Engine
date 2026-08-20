f734836ff1b85440fcbc947185a468e4048a695e
hiidkaboutyou-spec <hiidkaboutyou@gmail.com>
2026-08-20 22:12:22 +0330

    Add Rust CLI foundation

---
+++ src/cli.rs
@@ -0,0 +1,7 @@
+pub fn print_help() {
+    println!("Persian Literary Translation Engine");
+    println!("Commands:");
+    println!("  analyze <file>");
+    println!("  translate <project>");
+    println!("  export <chapter>");
+}
